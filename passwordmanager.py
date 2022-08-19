e0bed72e25575c6791040b34ab75152e68e50f7b
Felix Omollo <omollof418@gmail.com>
2022-08-19 16:31:41 +0300

    Made a skeleton of the log in detail

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,1 +1,19 @@
-#this is where i will write my code
+pwd = input("Enter the master password ")
+
+def view():
+    pass
+
+def add():
+    pass
+
+while True:
+    mode = input("Do you want to view or add a password?(view/add) To quit type q. ").lower()
+    if mode == "q":
+        break
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode")
+        continue
